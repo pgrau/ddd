aeda135baa7d98901a431932904b44ce4685de60
Pau <pau@mepas.es>
2016-04-02 00:17:04 +0200

    view login

--- src/Training/Infrastructure/UI/View/Plates/view/login/index.php
+++ src/Training/Infrastructure/UI/View/Plates/view/login/index.php
@@ -1,2 +1,10 @@
-<?php $this->layout('layout::index') ?>
-<div>Login</div>
+<?php $this->layout('layout::login') ?>
+<form class="form-signin">
+    <h2 class="form-signin-heading">Please sign in</h2>
+    <label for="inputEmail" class="sr-only">Email address</label>
+    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+</form>
