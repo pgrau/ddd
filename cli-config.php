b611f1939b424394c92dc40d6eddf41a37b5f5b5
Pau <pau@mepas.es>
2016-04-02 03:20:17 +0200

    config doctrine

--- cli-config.php
+++ cli-config.php
@@ -5,5 +5,5 @@ require_once __DIR__.'/vendor/autoload.php';
 $parameters =  require_once __DIR__.'/config/Parameters.php';
 
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(
-    (new \Mepas\Infrastructure\Persistence\Doctrine\EntityManagerFactory($parameters))->build()
+    (new \Training\Infrastructure\Persistence\Doctrine\EntityManagerFactory($parameters))->build()
 );
--- src/Training/Domain/Model/User/Identity/UserRepository.php
+++ src/Training/Domain/Model/User/Identity/UserRepository.php
@@ -4,9 +4,6 @@ namespace Training\Domain\Model\User\Identity;
 
 interface UserRepository
 {
-    /** @return User */
-    public function findByUsername($username);
-
     /** @return User|\Exception */
     public function findOneByUsername($username);
 }
