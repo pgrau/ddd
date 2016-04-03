0e3c331e68706a0ea3bf56a544e42dc082b09c44
Pau <pau@mepas.es>
2016-04-03 22:47:53 +0200

    db params are in sc

--- cli-config.php
+++ cli-config.php
@@ -2,8 +2,8 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$parameters = require __DIR__.'/config/Parameters.php';
+$container = \Training\Infrastructure\Service\Container\getContainer();
 
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(
-    (new \Training\Infrastructure\Persistence\Doctrine\EntityManagerFactory($parameters))->build()
+    (new \Training\Infrastructure\Persistence\Doctrine\EntityManagerFactory($container->get('db_params')))->build()
 );
