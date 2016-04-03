3e4eef43e44c37f65f222341a00ba7839ae8a727
Pau <pau@mepas.es>
2016-04-03 20:14:23 +0200

    add doctrine user repository

--- src/Training/Infrastructure/Persistence/Doctrine/User/Identity/DoctrineUserRepository.php
+++ src/Training/Infrastructure/Persistence/Doctrine/User/Identity/DoctrineUserRepository.php
@@ -3,12 +3,14 @@
 namespace Training\Infrastructure\Persistence\Doctrine\User\Identity;
 
 use Doctrine\ORM\EntityRepository;
+use Training\Domain\Model\User\Identity\User;
 use Training\Domain\Model\User\Identity\UserRepository;
 
 class DoctrineUserRepository extends EntityRepository implements UserRepository
 {
-    public function findOneByUsername($username)
+    public function persist(User $user)
     {
-        return $this->findOneBy([]);
+        $this->_em->persist($user);
+        $this->_em->flush();
     }
 }
